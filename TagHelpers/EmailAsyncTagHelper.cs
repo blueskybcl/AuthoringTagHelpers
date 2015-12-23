0ee0ab2e046c46d55b6c7711c20e5ca90ca75851
blueskybcl <a364711322@sina.com>
2015-12-23 09:04:33 +0800

    exercise

---
+++ TagHelpers/EmailAsyncTagHelper.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNet.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+    public class EmailAsyncTagHelper:TagHelper
+    {
+        private const string EmailDomain = "contoso.com";
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";    // Replaces <email> with <a> tag
+            //var content = await context.GetChildContentAsync();
+            //var target = content.GetContent() + "@" + EmailDomain;
+            //output.Attributes["href"] = "mailto:" + target;
+            //output.Content.SetContent(target);
+        }
+    }
+}
---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+    /// <summary>
+    /// 后缀TagHelper不是必须的，类名也可以命名为Email
+    /// </summary>
+    public class EmailTagHelper:TagHelper
+    {
+        private const string EmailDomain = "bcl.com";
+        public string MailTo { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var address = MailTo + "@" + EmailDomain;
+            output.Attributes["href"] = "mailto:" + address;
+            output.Content.SetContent(address);
+        }
+    }
+}
